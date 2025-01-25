fd46767d9cddb9ecba7cf1f12ec28a1441cf1844
billy <billy948787@gmail.com>
2025-01-25 16:30:11 +0800

    新增cmake

---
+++ build/Win-Loss_Relationship_Version/CMakeFiles/Win-Loss_Relationship_Version.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/lijiye/coding/TIC_TAC_TOE/Win-Loss_Relationship_Version/Game.cpp" "Win-Loss_Relationship_Version/CMakeFiles/Win-Loss_Relationship_Version.dir/Game.cpp.o" "gcc" "Win-Loss_Relationship_Version/CMakeFiles/Win-Loss_Relationship_Version.dir/Game.cpp.o.d"
+  "/Users/lijiye/coding/TIC_TAC_TOE/Win-Loss_Relationship_Version/MCTS.cpp" "Win-Loss_Relationship_Version/CMakeFiles/Win-Loss_Relationship_Version.dir/MCTS.cpp.o" "gcc" "Win-Loss_Relationship_Version/CMakeFiles/Win-Loss_Relationship_Version.dir/MCTS.cpp.o.d"
+  "/Users/lijiye/coding/TIC_TAC_TOE/Win-Loss_Relationship_Version/main.cpp" "Win-Loss_Relationship_Version/CMakeFiles/Win-Loss_Relationship_Version.dir/main.cpp.o" "gcc" "Win-Loss_Relationship_Version/CMakeFiles/Win-Loss_Relationship_Version.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/Win-Loss_Relationship_Version/CMakeFiles/Win-Loss_Relationship_Version.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Win-Loss_Relationship_Version.
+# This may be replaced when dependencies are built.
